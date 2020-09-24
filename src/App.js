6a425fbeece9743feffe285557ef82a575a1869e
subashchandarsiva <subashchandarsiva@gmail.com>
2020-09-24 21:22:49 +0100

    introduce the routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-// import "./App.css";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
+const Hats = () => {
+  return (
+    <div>
+      <h1>Hi Straw Hats</h1>
+    </div>
+  );
+};
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
